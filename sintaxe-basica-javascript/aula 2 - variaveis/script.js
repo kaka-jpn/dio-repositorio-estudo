b79dc5035323a7001db26c93921df393135e5d10
kaka-jp <kazuo.iwata@protonmail.com>
2021-12-18 20:41:20 -0300

    Inicia aula Sintaxe JavaScript

---
+++ sintaxe-basica-javascript/aula 2 - variaveis/script.js
@@ -0,0 +1,21 @@
+//tipos primitivos
+
+//boolean
+// var vOuf = false;
+// console.log(typeof(vOuf));
+
+// //number
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer));
+
+// //string
+// var nome = 'diana';
+// console.log(typeof(nome));
+
+//como declarar 
+var variavel = "diana";
+variavel = "martim";
+console.log(variavel);
+
+let variavel2 = "diana";
+console.log()
