9d3b3f4696bb058c05d37bc72452090fdf4f21ff
kaka-jp <kazuo.iwata@protonmail.com>
2021-12-23 19:59:55 -0300

    Aula variaveis do Sintaxe basica concluido

--- sintaxe-basica-javascript/aula 2 - variaveis/script.js
+++ sintaxe-basica-javascript/aula 2 - variaveis/script.js
@@ -1,21 +1,102 @@
 //tipos primitivos
 
 //boolean
-// var vOuf = false;
-// console.log(typeof(vOuf));
+/* var vOuf = false;
+console.log(typeof(vOuf));
 
 // //number
-// var numeroQualquer = 1;
-// console.log(typeof(numeroQualquer));
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
 
 // //string
-// var nome = 'diana';
-// console.log(typeof(nome));
+var nome = 'diana';
+console.log(typeof(nome));
+
+//function 
+var funcao = function(){}
+console.log(typeof(funcao)); */
 
 //como declarar 
-var variavel = "diana";
+/* var variavel = "diana";
 variavel = "martim";
 console.log(variavel);
 
 let variavel2 = "diana";
 console.log()
+variavel2 ="martino";
+console.log(variavel2); */
+
+/* const constante = "diana";
+constante = "martino";
+console.log(constante) */
+
+/* var escopoGlobal = "global";
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = "local";
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal(); */
+
+/* //atribuição
+var atribuicao = "diana";
+
+//comparação
+var comparacao = "0" == 0;
+console.log(comparacao);
+
+//comparação identica 
+var comparacaoIdentica = "0" === 0;
+console.log(comparacaoIdentica); */
+
+/* //adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+var potenciacao = 2 ** 10
+console.log(potenciacao); */
+
+/* //maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue); */
+
+/* //menor que
+var menorQue = 5 < 2;
+console.log(menorQue); */
+
+/* //maior ou igual
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual); */
+
+/* //menor ou igual
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual); */
+
+
+/* //operadores lógicos
+var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao); */
