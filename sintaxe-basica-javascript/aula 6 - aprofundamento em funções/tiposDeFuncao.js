c29ad19e7301143785d629a52c881782474fb37d
kaka-jp <kazuo.iwata@protonmail.com>
2022-01-05 11:33:13 -0300

    Conclui Aula sintaxe básica de JavaScript

---
+++ sintaxe-basica-javascript/aula 6 - aprofundamento em funções/tiposDeFuncao.js
@@ -0,0 +1,23 @@
+// tipos de função
+
+// declarativas
+/* function funcao() {
+    console.log("sou uma mensagem de uma função declarativa")
+}
+
+funcao(); */
+
+// expressão de função
+// com nomeação
+/* var funcao = function() {
+    console.log("sou uma mensagem de função de expressão")
+}
+
+funcao(); */
+
+//arrow function
+/* var funcao  = () => {
+    console.log("sou uma arrow function")
+}
+
+funcao(); */
